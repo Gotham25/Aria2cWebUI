47e6fd299ffb818ab320e6c5a53544e99a023493
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2020-03-17 05:08:30 +0000

    Added custom error handler, constants and utility classes

---
+++ Constants.php
@@ -0,0 +1,13 @@
+<?php
+    define("GID_FILE", "gids.log");
+    define("START_DAEMON", "startDaemon");
+    define("STOP_DAEMON", "stopDaemon");
+    define("DAEMON_STATUS", "aria2cDaemonStatus");
+    define("DOWNLOAD_DIRECTORY", sys_get_temp_dir());
+    define("GLOBAL_STATISTICS", "globalStat");
+    define("ADD_URI", "addUri");
+    define("TELL_ACTIVE", "tellActive");
+    define("TELL_WAITING", "tellWaiting");
+    define("TELL_STATUS", "tellStatus");
+    define("SET_GLOBAL_STATUS", "setGlobalOption");
+?>
