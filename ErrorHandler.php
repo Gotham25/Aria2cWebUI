5eff35961041c033826ddb7e05f7ef74ac857b93
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2020-03-17 05:08:30 +0000

    Added custom error handler, constants and utility classes

---
+++ ErrorHandler.php
@@ -0,0 +1,10 @@
+<?php
+    function handleError($errorNumber, $errorString, $errorFile, $errorLine) {
+        throw new Exception("<b>Error: </b>[$errorNumber] $errorString - $errorFile:$errorLine", -1);
+    }
+
+    class InsufficientArgumentException extends Exception { }
+    class UnsupportedRequestTypeException extends Exception { }
+    class GidFileProcessingException extends Exception { }
+    
+?>
