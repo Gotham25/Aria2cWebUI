24086303502a2755495e11697d1e5380c2c31f3e
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2020-03-25 16:31:13 +0530

    Fixed download link

--- downloadLogFile.php
+++ downloadLogFile.php
@@ -21,18 +21,20 @@ $absoluteFilePath .= (DIRECTORY_SEPARATOR . $filename);
 
 $absoluteFilePath = realpath($absoluteFilePath);
 
-// http headers for log file downloads
-header("Pragma: public");
-header("Expires: 0");
-header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-header("Cache-Control: public");
-header("Content-Description: File Transfer");
-header("Content-type: $contentType");
-header("Content-Disposition: attachment; filename=\"" . $filename . "\"");
-header("Content-Transfer-Encoding: binary");
-header("Content-Length: " . filesize($absoluteFilePath));
+echo PHP_EOL . "absoluteFilePath: $absoluteFilePath" . PHP_EOL . "filename: $filename" . PHP_EOL . "logFileName: $logFileName" . PHP_EOL . "fileType: $fileType" . PHP_EOL;
 
-ob_end_flush();
-@readfile($absoluteFilePath);
+// // http headers for log file downloads
+// header("Pragma: public");
+// header("Expires: 0");
+// header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+// header("Cache-Control: public");
+// header("Content-Description: File Transfer");
+// header("Content-type: $contentType");
+// header("Content-Disposition: attachment; filename=\"" . $filename . "\"");
+// header("Content-Transfer-Encoding: binary");
+// header("Content-Length: " . filesize($absoluteFilePath));
+
+// ob_end_flush();
+// @readfile($absoluteFilePath);
 
 ?>
